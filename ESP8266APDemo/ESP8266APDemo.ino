a84a302f0a73fa28f0adb0f71aad08acfc788e4e
Daniel Baek <baekj@rpi.edu>
2016-03-01 16:31:22 -0500

    Add Access Point Demo
    Demonstrate Access Point functionality.

---
+++ ESP8266APDemo/ESP8266APDemo.ino
@@ -0,0 +1,42 @@
+/****************
+ * AP Mode Demo *
+ ****************/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+// WiFi SSID and credentials.
+const char *ssid = "EnterTheESP";
+const char *password = "thereisnospoon";
+
+// Web server.
+ESP8266WebServer server(80);
+
+unsigned long responses_sent = 0;
+
+void handleRoot(){
+  responses_sent++;
+  server.send(200, "text/html", "Congratulations! You are the " + string(responses_sent) +
+                                "user!");
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP Address: ");
+  Serial.println(myIP);
+
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started!");
+}
+
+void loop() {
+  server.handleClient();
+}
