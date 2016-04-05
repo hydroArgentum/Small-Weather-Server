8b0da4602a8f9e4dfdbaebf0474470203d8a50d4
Daniel Baek <baekj@rpi.edu>
2016-04-05 16:34:12 -0400

    Add WiFi Functions and Diagnostics
    Needs testing. However, the nightly build of Arduino seems to have
    trouble with the WiFi libraries. Pushing this now so that it can be
    tested later with the stable version.

--- ESP8266Demo.ino
+++ ESP8266Demo.ino
@@ -2,6 +2,8 @@
  * ESP8266 Demo *
  ****************/
 // Libraries included.
+// Arduino base WiFi library.
+#include <WiFi.h>
 // ESP8266 WiFi and Web Server.
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
@@ -16,14 +18,13 @@
 #define BME280_CS 5
 // SD chip select pin.
 #define SD_CS 4
-
 // WiFi information. ESP8266 supports WPA2-PSK.
 // ESP8266 should support WEP, WPA.
 // WPA2-Enterprise + EAP-TLS should be supported, but WPA2-Enterprise + PEAP.
 // There might be a workaround for authentication based on eduroam.
 // We probably won't use eduroam just to do it though.
-const char* ssid = "esp8266demo";
-const char* password = "rcos";
+#define WIFI_SSID "esp8266demo"
+#define WIFI_KEY "opensource"
 
 // BME280 module.
 Adafruit_BME280 bme280(BME280_CS);
@@ -49,11 +50,39 @@ void BME280_setup(){
   Serial.println("BME280 connection complete!");
 }
 
+void WiFi_setup(){
+  // Setup WIFI.
+  WiFi.begin(WIFI_SSID, WIFI_KEY);
+  // Wait for connection.
+  while (WiFi.status() != WL_CONNTECTED){
+    Serial.print(".");
+  }
+  // Connection completed.
+  Serial.println();
+  Serial.println("WiFi connection complete!");
+}
+
+void WiFi_diagnostics(){
+  // WiFi diagnostics.
+  Serial.print("Connected to: ");
+  Serial.println(ssid);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Subnet Mask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway Address: ");
+  Serial.println(WiFi.gatewayIP());
+}
+
 void setup() {
   // Start UART communication.
   serial_setup();
   // Setup BME280.
   BME280_setup();
+  // Setup WiFi.
+  WiFi_setup();
+  // Check WiFi.
+  WiFi_diagnostics();
 }
 
 void loop() {
